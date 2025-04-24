f4d0188922e59eefbefc81cfd5a37c2f5e9b9d2a
sayali sasane <sayalirsasane16@gmail.com>
2025-04-24 23:20:04 +0530

    sign up changed

--- src/page/signup.jsx
+++ src/page/signup.jsx
@@ -1,91 +1,3 @@
-// import React, { useState } from 'react';
-// import './signup.css';
-// import { useNavigate } from "react-router-dom";
-
-// const Register = () => {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     email: '',
-//     password: '',
-//   });
-
-//   const [error, setError] = useState('');
-//   const [success, setSuccess] = useState('');
-//   const navigate = useNavigate();
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData((prev) => ({
-//       ...prev,
-//       [name]: value,
-//     }));
-//     setError('');
-//     setSuccess('');
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     const users = JSON.parse(localStorage.getItem('users')) || [];
-//     const userExists = users.find(user => user.email === formData.email);
-
-//     if (userExists) {
-//       setError('User already exists with this email');
-//     } else {
-//       users.push(formData);
-//       localStorage.setItem('users', JSON.stringify(users));
-//       alert('Registration successful!');
-//       setFormData({ name: '', email: '', password: '' });
-//     }
-//   };
-
-//   return (
-//     <div className="register-container">
-//       <h2>Create an Account</h2>
-//       {error && <p className="error-msg">{error}</p>}
-//       {success && <p className="success-msg">{success}</p>}
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           name="name"
-//           placeholder="Full Name"
-//           value={formData.name}
-//           onChange={handleChange}
-//           required
-//         />
-//         <input
-//           type="email"
-//           name="email"
-//           placeholder="Email Address"
-//           value={formData.email}
-//           onChange={handleChange}
-//           required
-//         />
-//         <input
-//           type="password"
-//           name="password"
-//           placeholder="Create Password"
-//           value={formData.password}
-//           onChange={handleChange}
-//           required
-//           minLength={6}
-//         />
-//         <button type="submit">Register</button>
-//           <p>Already have an account? <a href="/login">Login</a></p>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Register;
-
-
-
-
-
-
-
-
 
 
 
@@ -141,7 +53,7 @@ const Register = () => {
     //   });
     // }
 
-     axios.post('http://garland.mohitsasane.tech/api/users/register',formData).then(response=>{
+     axios.post('http://garland.mohitsasane.tech/backend/api/users/register',formData).then(response=>{
       console.log("post created :",response.data);
       // alert("Sign up successful..");
       toast.success("Signup successful!", { autoClose: 2000 });
@@ -181,9 +93,9 @@ const Register = () => {
         />
         <input
           type="password"
-          name="password"
+          name="pass"
           placeholder="Create Password"
-          value={formData.password}
+          value={formData.pass}
           onChange={handleChange}
           required
           minLength={6}
