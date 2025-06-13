411c5046c8b44e2c810e830afe3f72d911e30a85
Sayali <sayali@MacBookAir.lan>
2025-06-13 23:03:05 +0530

    te

--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -6,7 +6,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './Admin.css';
+import './admin.css';
 
 
 const AdminPanel = () => {
